a3ba3af1b5177713909fa1e81c3a86a559fdb813
zitouni-bilal <z.bilal89@gmail.com>
2017-12-01 17:28:14 +0100

    ajout projet

---
+++ ProjetPAM/src/main/java/org/pam/controllers/test.java
@@ -0,0 +1,7 @@
+package org.pam.controllers;
+
+import java.io.Serializable;
+
+public class test implements Serializable {
+
+}
---
+++ ProjetPAM/src/test/java/org/pam/ProjetPamApplicationTests.java
@@ -0,0 +1,16 @@
+package org.pam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetPamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
