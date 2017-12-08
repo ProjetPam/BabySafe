ca598ce8780808143417c69acfb91b4718506815
ilyassR <ilyass.rahmoune@gmail.com>
2017-12-08 21:17:43 +0100

    Test commit

--- ProjetPAM/src/main/java/org/pam/ProjetPamApplication.java
+++ ProjetPAM/src/main/java/org/pam/ProjetPamApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetPamApplication {
-
+	/*Spring boot run application class*/
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetPamApplication.class, args);
 	}
